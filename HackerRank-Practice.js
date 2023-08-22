56f0dc4af2fcf7594079dc69676a2d3352692d2b
BobbyLaGagne <simondeclairfayt55@gmail.com>
2023-08-22 18:10:08 +0200

    Added a new problem

--- HackerRank-Practice.js
+++ HackerRank-Practice.js
@@ -51,3 +51,35 @@ function diagonalDifference(arr) {
     return diff(leftDiagonal, rightDiagonal);
 
 }
+//SUBARRAY DIVISION 1
+function birthday(s, d, m) {
+    // Créer une array qui a la même valeur que l'array de base
+    let num = s;
+    //Faire une array vide dans laquelle on va mettre des num
+    let nums = [];
+    let count = 0;
+    //Fonction qui additionne tous les nombres de l'array
+    const add = (arr) => arr.reduce((a, b) => a + b, 0);
+    for (let i = 0; i < s.length; i++) {
+        //Créer un array element qui aura comme valeur une array allant de (0+i, m+i)
+      let arrayElement = num.slice(0 + i, m + i);
+      //Push cet element dans l'array vide
+      nums.push(arrayElement);
+    }
+    //Si il n'y a qu'un element et qu'il correspond à D, ajouter 1 au compte
+    if(num.length===1 && num[0]===d){
+        count++;
+    }else{
+        //Pour chaque el, vérifie si la somme de l'element == d
+        //Si oui, ajouter un au compte
+      nums.forEach((el) => {
+          if (add(el) === d) {
+            count++;
+          }
+        });
+    }
+    
+  
+    return count;
+}
+console.log(birthday(s, d, m));
